e3629a5b56222c93999bfaa00a82877920878c41
Alina <wbrflf3301@outlook.com>
2022-12-26 01:15:33 +0500

    ощфщзощыпы

--- java-task/src/Program.java
+++ java-task/src/Program.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Program {
+    final static Path rscdirectory = Paths.get("/rsc");
+    public static void Main()
+    {
+        if(Files.exists(rscdirectory) && Files.isDirectory(rscdirectory))
+            try (DirectoryStream<Path> dirStream =
+                         Files.newDirectoryStream(rscdirectory)) {
+
+            } catch (IOException e) {
+                throw new RuntimeException("Такого файла или директории не существует");
+            }
+    }
 }
